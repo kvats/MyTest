77623a0aac5ea07fffaa64c5d550aa88f9a9a5ef
kvats <kvats@impetus-NL183.impetus.co.in>
2014-08-27 14:27:36 +0530

    modified packaging for ActionSave.

--- design-patterns/src/main/java/com/impetus/designpattern/command/impl/ActionSave.java
+++ design-patterns/src/main/java/com/impetus/designpattern/command/impl/ActionSave.java
@@ -1,23 +1,22 @@
-package com.impetus.designpattern.behavioural.command.impl;
+package com.impetus.designpattern.command.impl;
 
-import com.impetus.designpattern.behavioural.command.interfaces.ActionListenerCommand;
-
+import com.impetus.designpattern.command.interfaces.ActionListenerCommand;
 
 /**
-Concrete Command:
-*/
+ * Concrete Command:
+ */
 public class ActionSave implements ActionListenerCommand {
 
-	private Document adoc;
+    private Document adoc;
 
-	public ActionSave(Document doc) {
-		this.adoc = doc;
-	}
+    public ActionSave(Document doc) {
+        this.adoc = doc;
+    }
 
-	@Override
-	public void execute() {
-		adoc.Save();
+    @Override
+    public void execute() {
+        adoc.Save();
 
-	}
+    }
 
 }
