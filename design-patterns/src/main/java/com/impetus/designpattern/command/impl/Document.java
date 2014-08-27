7a1e0a1a669635ee87cf171de96aa319dcaf2d03
kvats <kvats@impetus-NL183.impetus.co.in>
2014-08-27 14:28:42 +0530

    modified packaging for Document.java

--- design-patterns/src/main/java/com/impetus/designpattern/command/impl/Document.java
+++ design-patterns/src/main/java/com/impetus/designpattern/command/impl/Document.java
@@ -1,15 +1,17 @@
 /**
  * 
  */
-package com.impetus.designpattern.behavioural.command.impl;
+package com.impetus.designpattern.command.impl;
+
 /**
-Receiver class:
-*/
+ * Receiver class:
+ */
 public class Document {
-   public void Open(){
-	   System.out.println("Document Opened");
-   }
-   public void Save(){
-	   System.out.println("Document Saved");
-   }
+    public void Open() {
+        System.out.println("Document Opened");
+    }
+
+    public void Save() {
+        System.out.println("Document Saved");
+    }
 }
